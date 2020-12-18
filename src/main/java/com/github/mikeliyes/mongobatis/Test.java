80515919f51233ead4fef41668e2a938a1bbd131
lidebing <lidebing@qq.com>
2020-12-18 23:58:12 +0800

    add config.xml

---
+++ src/main/java/com/github/mikeliyes/mongobatis/Test.java
@@ -0,0 +1,11 @@
+package com.github.mikeliyes.mongobatis;
+
+public class Test {
+
+	public static void main(String[] args) {
+//		String resource = "org/mybatis/example/mybatis-config.xml";
+//		InputStream inputStream = Resources.getResourceAsStream(resource);
+//		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
+
+}
